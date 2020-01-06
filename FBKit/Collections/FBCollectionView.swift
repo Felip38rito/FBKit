fafc13816a9f75765bc89cd147618fade3e68b50
Felip38rito <felipe.correia.wd@gmail.com>
2020-01-06 20:54:33 -0300

    Add filter functionality to FBCollectionView

--- FBKit/Collections/FBCollectionView.swift
+++ FBKit/Collections/FBCollectionView.swift
@@ -12,16 +12,18 @@ public class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectio
     
     private weak var collectionView: UICollectionView!
     private var listData = [ViewModel]()
+    private var filteredData = [ViewModel]()
     
     /// Create a UICollectionView with MVVM strategy
     /// - Parameter viewModelList: A FBCellViewModel list to be translated into UICollectionViewCell's Models
     /// - Parameter collectionView: An UICollectionView instance to be filled
-    init(list: [ViewModel], collectionView: UICollectionView) {
+    public init(list: [ViewModel], collectionView: UICollectionView) {
         super.init()
         
-        self.collectionView = collectionView
-        self.listData = list
+        listData = list
+        filteredData = listData
         
+        self.collectionView = collectionView
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
     }
@@ -29,19 +31,20 @@ public class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectio
     /// Add new items to the UICollectionView
     public func add(_ items: [ViewModel]) {
         self.listData += items
+        self.filteredData = listData
         self.collectionView.reloadData()
     }
     
     // MARK: - DataSource and Delegate methods
     /// The items are inside listData
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return listData.count
+        return filteredData.count
     }
     
     /// Each row generated for collection view is based on viewmodel properties
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let vm = listData[indexPath.row]
+        let vm = filteredData[indexPath.row]
         vm.prepareView(view: collectionView.dequeueReusableCell(withReuseIdentifier: vm.identifier, for: indexPath))
         
         return vm.view as! UICollectionViewCell
@@ -49,7 +52,12 @@ public class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectio
     
     /// The size of the cell component come from ViewModel
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return listData[indexPath.row].viewSize
+        return filteredData[indexPath.row].viewSize
     }
     
+    /// Filter data to match some generic condition 
+    public func filter(success: @escaping ((_ vm: ViewModel) -> Bool)) {
+        self.filteredData = listData.filter(success)
+        self.collectionView.reloadData()
+    }
 }
