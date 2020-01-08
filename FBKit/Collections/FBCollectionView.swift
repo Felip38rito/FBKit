589adc7172a07249c6e5994674f487783a3666e1
Felip38rito <felipe.correia.wd@gmail.com>
2020-01-08 15:31:10 -0300

    IndexDelegate capability

--- FBKit/Collections/FBCollectionView.swift
+++ FBKit/Collections/FBCollectionView.swift
@@ -14,6 +14,9 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
     internal var original_components_list = [ViewModel]()
     internal var filtered_components_list = [ViewModel]()
     
+    /// The delegate responsible to catch index visibility (for use in pagination for example)
+    public var indexDelegate: FBIndexDelegate?
+    
     /// Create a UICollectionView with MVVM strategy
     /// - Parameter viewModelList: A FBCellViewModel list to be translated into UICollectionViewCell's Models
     /// - Parameter collectionView: An UICollectionView instance to be filled
@@ -26,27 +29,6 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
         self.collectionView = collectionView
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
-    }
-    
-    /// The list count when used filter (get all if no filter is in use)
-    public var filtered_count: Int {
-        get {
-            return filtered_components_list.count
-        }
-    }
-    
-    /// The list object count regardless of filter
-    public var list_count: Int {
-        get {
-            return original_components_list.count
-        }
-    }
-    
-    /// Add new items to the UICollectionView
-    public func add(_ items: [ViewModel]) {
-        self.original_components_list += items
-        self.filtered_components_list = original_components_list
-        self.collectionView.reloadData()
     }
     
     // MARK: - DataSource and Delegate methods
@@ -69,9 +51,38 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
         return filtered_components_list[indexPath.row].viewSize
     }
     
+    /// Expose the indexPath of the cell to be displayed
+    public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        indexDelegate?.willReachIndex(index: indexPath)
+    }
+    
+    // MARK: - Custom functionality
     /// Filter data to match some generic condition 
     public func filter(success: @escaping ((_ vm: ViewModel) -> Bool)) {
         self.filtered_components_list = original_components_list.filter(success)
         self.collectionView.reloadData()
+    }
+    
+    /// Add new items to the UICollectionView
+    public func add(_ items: [ViewModel]) {
+        self.original_components_list += items
+        self.filtered_components_list = original_components_list
+        self.collectionView.reloadData()
+    }
+    
+    // MARK: - Custom properties
+    /// The list count when used filter (get all if no filter is in use)
+    public var filtered_count: Int {
+        get {
+            return filtered_components_list.count
+        }
+    }
+    
+    /// The list object count regardless of filter
+    public var list_count: Int {
+        get {
+            return original_components_list.count
+        }
     }
 }
---
+++ FBKit/Collections/FBIndexDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  FBIndexDelegate.swift
+//  FBKit
+//
+//  Created by Felipe Correia on 08/01/20.
+//  Copyright © 2020 Felip38rito. All rights reserved.
+//
+
+import Foundation
+
+public protocol FBIndexDelegate {
+    func willReachIndex(index: IndexPath)
+}
