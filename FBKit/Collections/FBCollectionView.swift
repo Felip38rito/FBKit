f8ccb43c3b51fd93828577c488d8262f19386539
Felip38rito <felipe.correia.wd@gmail.com>
2020-01-08 13:45:45 -0300

    Internal properties to subclasses

--- FBKit/Collections/FBCollectionView.swift
+++ FBKit/Collections/FBCollectionView.swift
@@ -11,8 +11,9 @@ import UIKit
 open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
     
     private weak var collectionView: UICollectionView!
-    private var listData = [ViewModel]()
-    private var filteredData = [ViewModel]()
+    internal var original_components_list = [ViewModel]()
+    /// The filtered object list that can be accessed in subclasses
+    internal var filtered_components_list = [ViewModel]()
     
     /// Create a UICollectionView with MVVM strategy
     /// - Parameter viewModelList: A FBCellViewModel list to be translated into UICollectionViewCell's Models
@@ -20,8 +21,8 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
     public init(list: [ViewModel], collectionView: UICollectionView) {
         super.init()
         
-        listData = list
-        filteredData = listData
+        original_components_list = list
+        filtered_components_list = original_components_list
         
         self.collectionView = collectionView
         self.collectionView.delegate = self
@@ -30,21 +31,21 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
     
     /// Add new items to the UICollectionView
     public func add(_ items: [ViewModel]) {
-        self.listData += items
-        self.filteredData = listData
+        self.original_components_list += items
+        self.filtered_components_list = original_components_list
         self.collectionView.reloadData()
     }
     
     // MARK: - DataSource and Delegate methods
     /// The items are inside listData
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return filteredData.count
+        return filtered_components_list.count
     }
     
     /// Each row generated for collection view is based on viewmodel properties
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let vm = filteredData[indexPath.row]
+        let vm = filtered_components_list[indexPath.row]
         vm.prepareView(view: collectionView.dequeueReusableCell(withReuseIdentifier: vm.identifier, for: indexPath))
         
         return vm.view as! UICollectionViewCell
@@ -52,12 +53,12 @@ open class FBCollectionView<ViewModel: FBCellViewModel>: NSObject, UICollectionV
     
     /// The size of the cell component come from ViewModel
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return filteredData[indexPath.row].viewSize
+        return filtered_components_list[indexPath.row].viewSize
     }
     
     /// Filter data to match some generic condition 
     public func filter(success: @escaping ((_ vm: ViewModel) -> Bool)) {
-        self.filteredData = listData.filter(success)
+        self.filtered_components_list = original_components_list.filter(success)
         self.collectionView.reloadData()
     }
 }
