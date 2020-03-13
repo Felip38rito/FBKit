fb0a682a4d42fe03796ddb4aef850713020bb8bc
Felip38rito <felipe.correia.wd@gmail.com>
2020-03-13 15:37:56 -0300

    Faz parte....

--- FBKit/View/FBView.swift
+++ FBKit/View/FBView.swift
@@ -140,7 +140,7 @@ import UIKit
             self.layer.shadowOpacity = Float(shadowOpacity)
         }
         
-        self.layer.frame = self.bounds
+        self.layer.frame = self.frame
         
         self.layer.insertSublayer(gradientBackgroundLayer, at: 0)
         self.layer.insertSublayer(shapeBorderLayer, at: 1)
